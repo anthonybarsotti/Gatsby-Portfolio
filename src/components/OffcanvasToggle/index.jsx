b1353e362f49ef7025a2eedfd1118387a49992f8
Anthony <anthonydbarsotti@gmail.com>
2019-06-06 22:34:58 -0400

    Adding mobile styles for header

--- src/components/OffcanvasToggle/index.jsx
+++ src/components/OffcanvasToggle/index.jsx
@@ -3,23 +3,23 @@ import styles from "./index.module.scss";
 
 export default function OffcanvasToggle(props) {
 	const iconLines = Array.from({ length: 4 }).map((_, i) => {
-		const lineStyles = {
+		const computedStyles = {
 			backgroundColor: props.iconColor
 		};
 
 		// Add opacity for first and last
 		if (i === 0 || i === 3) {
-			lineStyles.opacity = props.active ? 0 : 1;
+			computedStyles.opacity = props.active ? 0 : 1;
 		} else if(i === 1) {
-			lineStyles.transform = `translate(-50%, -50%) rotate(${ props.active ? 45 : 0 }deg)`;
+			computedStyles.transform = `translate(-50%, -50%) rotate(${ props.active ? 45 : 0 }deg)`;
 		} else {
-			lineStyles.transform = `translate(-50%, -50%) rotate(${ props.active ? -45 : 0 }deg)`;
+			computedStyles.transform = `translate(-50%, -50%) rotate(${ props.active ? -45 : 0 }deg)`;
 		}
 
 		return <span
 			key={ `icon-line-${i}` }
-			className={ styles.iconLine }
-			style={ lineStyles }
+			className={ styles.line }
+			style={ computedStyles }
 		/>
 	});
 
